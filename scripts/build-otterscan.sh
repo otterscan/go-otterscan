1c48c923e6485f66ce8377d5df109959c6a2447f
Willian Mitsuda <wmitsuda@gmail.com>
2023-08-04 15:29:34 -0300

    Remove all brotli and plain .js/css assets that are available in .gz form

--- scripts/build-otterscan.sh
+++ scripts/build-otterscan.sh
@@ -5,3 +5,11 @@
 # Those files are meant to be commited in source control.
 cd ../otterscan
 npm ci && npm run build && rm -fr ../ots/dist && cp -R dist ../ots/dist
+
+# Remove all .js|css files that have a corresponding precompressed .js|css.gz
+cd ../ots/dist
+find . -name "*.js.gz" | rev | cut -c 4- | rev | cat | xargs rm
+find . -name "*.css.gz" | rev | cut -c 4- | rev | cat | xargs rm
+
+# Remove all .br precompressed brotli files
+rm assets/*.br
